d12edced91ba442f9f952aea0a810388160520e3
sourabh104 <100758383+sourabh104@users.noreply.github.com>
2022-03-02 17:46:44 +0530

    Add files via upload

---
+++ Nettantra/static.js
@@ -0,0 +1,151 @@
+var team_data = [
+
+    {
+        "team_no": 1,
+        "teamFullName": "Royal Challenges Banglore",
+        "teamSortName": "RCB",
+        "teamLogo": "rcb.jpg",
+        "topBatsMan": "virat Kohil,Dhoni",
+        "topBolower": "tendhulkar",
+        "totalPlayer": 20,
+        "wonMatch": 5,
+        "player": [
+            {
+                "name": "Virat Kohil",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "AB Devileaus",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Sachini Tendulkar",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+    },
+    {
+        "team_no": 2,
+        "teamFullName": "Mumbai Indians",
+        "teamSortName": "Mi",
+        "teamLogo": "mi.jpg",
+        "topBatsMan": "Rohit sarma",
+        "topBolower": "hardik pandia",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Virat Kohil",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "AB Devileaus",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Sachini Tendulkar",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+    },
+    {
+        "team_no": 3,
+        "teamFullName": "Chennai Super King",
+        "teamSortName": "CSK",
+        "teamLogo": "csk.jpg",
+        "topBatsMan": "Dhoni",
+        "topBolower": "hardik pandia",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Virat Kohil",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "AB Devileaus",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Sachini Tendulkar",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+
+    },
+    {
+        "team_no": 4,
+        "teamFullName": "Panjab",
+        "teamSortName": "PNB",
+        "teamLogo": "pnb.jpg",
+        "topBatsMan": "Dhoni",
+        "topBolower": "hardik pandia",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Virat Kohil",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "AB Devileaus",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Sachini Tendulkar",
+                "photo": "virat.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+
+    }
+]
+
+localStorage.setItem('team_data', JSON.stringify(team_data))
